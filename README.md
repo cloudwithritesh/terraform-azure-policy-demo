6ef19bfcde4ca314f9132de97cec021cd50ae7b1
cloudwithritesh <ritesh.hegde@hotmail.com>
2025-04-16 22:55:31 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # terraform-azure-policy-demo
+# Terraform Azure Policy Demo
+
+This demo enforces a custom Azure Policy that requires an `Environment` tag on all resources using Terraform.
+
+## 💡 Components
+- Custom Azure Policy (JSON)
+- Terraform to create and assign the policy
+- Demo flow to show governance in action
+
+## 🚀 How to Run
+
+```bash
+terraform init
+terraform plan
+terraform apply
---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "demo_rg" {
+  name     = "rg-policy-demo"
+  location = var.location
+}
+
+resource "azurerm_policy_definition" "require_env_tag" {
+  name         = "require-env-tag"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Require Environment Tag"
+  description  = "Enforces the existence of an 'Environment' tag on all resources."
+
+  policy_rule = file("${path.module}/policy/require-tags-policy.json")
+}
+
+resource "azurerm_policy_assignment" "require_env_tag_assignment" {
+  name                 = "require-env-tag-assignment"
+  policy_definition_id = azurerm_policy_definition.require_env_tag.id
+  scope                = azurerm_resource_group.demo_rg.id
+  description          = "Assignment of require-env-tag policy"
+  display_name         = "Require Environment Tag Assignment"
+}
