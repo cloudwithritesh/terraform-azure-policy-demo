2e045bcc27f61b9c115b72acc387089623b9d740
Ritesh Hegde <ritesh.hegde@gmail.com>
2025-04-20 12:29:26 +0800

    update-2

--- providers.tf
+++ providers.tf
@@ -1,27 +1,27 @@
-terraform {
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.47.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatedemo190425"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
+# terraform {
+#   required_providers {
+#     azuread = {
+#       source  = "hashicorp/azuread"
+#       version = "~> 2.47.0"
+#     }
+#   }
+#   backend "azurerm" {
+#     resource_group_name  = "tfstate-rg"
+#     storage_account_name = "tfstatedemo190425"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+# }
 
-provider "azuread" {}
+# provider "azuread" {}
 
-provider "azurerm" {
-  features {}
-  # Option 1: Explicitly set subscription_id
-  # subscription_id = var.subscription_id
+# provider "azurerm" {
+#   features {}
+#   # Option 1: Explicitly set subscription_id
+#   # subscription_id = var.subscription_id
 
-  # Option 2: Use environment variable ARM_SUBSCRIPTION_ID
-  use_oidc = true
-}
+#   # Option 2: Use environment variable ARM_SUBSCRIPTION_ID
+#   use_oidc = true
+# }
 
-data "azurerm_subscription" "current" {}
+# data "azurerm_subscription" "current" {}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "location" {
-  type    = string
-  default = "Southeast Asia"
-}
+# variable "location" {
+#   type    = string
+#   default = "Southeast Asia"
+# }
 
 # variable "subscription_id" {
 #   type = string
